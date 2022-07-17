371de7af9acb1eb8105a9e59b8536cb914c9af76
arialghifari <arialghifari99@gmail.com>
2022-07-18 00:43:25 +0800

    Color components with RTK Query

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
+import ColorForm from "./container/ColorForm";
+import ColorList from "./container/ColorList";
 import Wallet from "./container/Wallet";
 
 function App() {
   return (
     <div className="App">
       <Wallet />
+      <ColorList />
+      <ColorForm />
     </div>
   );
 }
--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import walletSlice from "../reducers/walletSlice";
+import { colorApi } from "../services/colorsApi";
 
 const store = configureStore({
   reducer: {
     wallet: walletSlice,
+    [colorApi.reducerPath]: colorApi.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(colorApi.middleware),
 });
 
 export default store;
